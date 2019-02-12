f640e97a7efd1a3ad511a7efc6199b2285b39dbd
igor <perrofeofilms@gmail.com>
2019-02-12 09:31:53 -0600

    -

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
 
 
       <b-navbar-brand href="#">
-        <img class="rounded-circle" src="~assets/logo_perrofeofilms_amarillo.jpg" title="jaú!" alt="perrofeo logo">
+        <img class="rounded-circle" src="~assets/logo_perrofeofilms_amarillo.jpg" title="jau!" alt="perrofeo logo">
       </b-navbar-brand>
 
     </b-navbar>  
